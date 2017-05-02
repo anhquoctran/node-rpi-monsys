1b0d9cd622237dbb03370dce2df9379f8128456e
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2017-05-03 00:43:29 +0700

    add resources assets for angular

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,29 @@
 var express = require('express')
+var crypto = require('crypto'),
+    text = 'hello',
+    key = 'abcfdfsdf'
 
 module.exports = function Route(app, passport) {
     app.get('/', function(req, res) {
         var fullUrl = req.protocol + '://' + req.get('host');
-        res.render("work", {
-            server: fullUrl
+        var hash = crypto.createHash('sha512')
+        hash.update(new Date().toLocaleTimeString())
+        var key = hash.digest('hex')
+
+        var hashtoken = crypto.createHash('sha256')
+        hashtoken.update(key)
+        var token = hashtoken.digest('hex')
+
+        res.json({
+            message: "The API is running on " + fullUrl,
+            status: 200,
+            current_time: new Date().toLocaleTimeString(),
+            session_base: {
+                key: key,
+                token: token,
+                bare: req.session
+            },
+            data: req.route || null
         })
     })
 }
