a9462d058d50fd24e292cd661efde262ace8a8e6
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2017-05-04 01:23:37 +0700

    add security

---
+++ app/middleware/security.js
@@ -0,0 +1,18 @@
+const crypto = require('crypto')
+
+function Security() {
+    this.encryptPassword = function() {
+
+    }
+
+    this.createHash = function() {
+
+    }
+
+    this.createRSA = function() {
+
+    }
+
+}
+
+module.exports = new Security();
