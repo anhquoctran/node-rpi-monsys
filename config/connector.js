98b0dbcb431149a4064b13bfc7e7e6a463944b10
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2017-05-05 00:14:16 +0700

    fix database

--- config/connector.js
+++ config/connector.js
@@ -7,11 +7,13 @@ function Connector() {
         This is database connector module to connect to MySQL Database
     */
     this.connector = mysql.createConnection(connection.MySqlDbConnection)
+    console.log("Connecting to database... ")
     this.connector.connect(function(error) {
-        if (err) {
-            console.error("Error when connecting to MySQL Database: " + err.stack);
+        if (error) {
+            console.error("Error when connecting to MySQL Database: " + error.stack);
             return;
         }
+        console.log("Database connection has been established successfully!")
         require('../database/init')
     })
 
