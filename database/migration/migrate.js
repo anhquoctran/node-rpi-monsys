dfefd4e9a8361f4770dfffac57773cff1acff79e
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2017-05-05 17:06:33 +0700

    completed migrate functions

--- database/migration/migrate.js
+++ database/migration/migrate.js
@@ -26,7 +26,7 @@ function Migrate() {
         })
     }
 
-    this.getOneUser = function(usernameOrEmail, password) {
+    this.login = function(usernameOrEmail, password) {
         return new Promise(function(resolve, reject) {
             mysql.connector.query("call procGetUsernameOrEmail(?, ?)", [usernameOrEmail, password], function(error, user) {
                 if (error) reject(error)
@@ -41,12 +41,27 @@ function Migrate() {
         })
     }
 
+    this.getOneUser = function(username) {
+        return new Promise(function(resolve, reject) {
+            mysql.connector.query("call procGetOneUserByUsername(?)", username, function(error, result) {
+                if (error) reject(error)
+                else {
+                    if (result.length > 0) {
+                        resolve(result)
+                    } else {
+                        resolve(null)
+                    }
+                }
+            })
+        })
+    }
+
     this.getAdmin = function() {
         return new Promise(function(resolve, reject) {
             mysql.connector.query("call getUserIfAdmin()", function(error, users) {
                 if (error) reject(error)
                 else {
-                    if (!users) {
+                    if (!users || users.length < 0) {
                         resolve(null)
                     } else {
                         resolve(users)
@@ -89,7 +104,13 @@ function Migrate() {
     this.updateUser = function(fullname, email, phone, birthdate, hometown, wherenow, bio, description) {
         return new Promise(function(resolve, reject) {
             mysql.connector.query("update rpi_user set fullname = ?, email = ?, phone = ?, birthdate = ?, hometown = ?, wherenow = ?, bio = ?, description = ?", [fullname, email, phone, birthdate, hometown, wherenow, bio, description], function(error, result) {
-
+                if (error) resolve(error)
+                else {
+                    if (result.affectedRows >= 0)
+                        resolve(true)
+                    else
+                        resolve(false)
+                }
             })
         })
     }
@@ -111,7 +132,7 @@ function Migrate() {
 
     this.updatePassword = function(username, newpassword) {
         return new Promise((resolve, reject) => {
-            mysql.connector.query("update rpi_user set password = ? where username like ?", [password, username], function(error, result) {
+            mysql.connector.query("update rpi_user set password = ? where username like ?", [newpassword, username], function(error, result) {
                 if (error) reject(error)
                 else {
                     if (result.affectedRows >= 0) {
@@ -124,13 +145,56 @@ function Migrate() {
 
     this.saveSession = function(name, key, token, datetime, client, mac, path) {
         return new Promise(function(resolve, reject) {
-
+            mysql.connector.query("insert into rpi_session(name, key, token, datetime, client, mac, path) values(?, ?, ?, ?, ?, ?, ?)", [name, key, token, datetime, client, mac, path], function(error, result) {
+                if (error) reject(error)
+                else {
+                    if (result.affectedRows >= 0) {
+                        resolve(true)
+                    } else {
+                        resolve(false)
+                    }
+                }
+            });
         })
     }
 
-    this.listSession = function() {
+    this.listSessionFromDate = function(datefrom) {
         return new Promise(function(resolve, reject) {
+            mysql.connector.query("select S.id, S.name, S.token, S.datetime, S.client, S.mac, S.path from rpi_sessions S where datetime between ? and now()", datefrom, function(error, result) {
+                if (error) reject(error)
+                else {
+                    if (result.length > 0) {
+                        resolve(result)
+                    } else resolve(null)
+                }
+            })
+        })
+    }
 
+    this.listSessionFromDateToDate = function(from, to) {
+        return new Promise(function(resolve, reject) {
+            mysql.connector.query("select S.id, S.name, S.token, S.datetime, S.Client, S.mac, S.path from rpi_sessions S where datetime between ? and ?", [from, to], function(error, result) {
+                if (error) reject(error)
+                else {
+                    if (result.length > 0) {
+                        resolve(result)
+                    } else resolve(null)
+                }
+            })
+        })
+    }
+
+    this.countSession = function() {
+        return new Promise(function(resolve, reject) {
+            mysql.connector.query("select COUNT(*) from rpi_sessions", function(error, result) {
+                if (error) reject(error)
+                else {
+                    if (result["COUNT(*)"]) {
+                        var data = result["COUNT(*)"]
+                        resolve(data)
+                    }
+                }
+            })
         })
     }
 
