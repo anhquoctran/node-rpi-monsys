da1f2cc5501c1845144ee2cd6b541c7ca154e550
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2017-05-14 13:23:50 +0700

    fix routes

--- app/routes.js
+++ app/routes.js
@@ -235,7 +235,7 @@ module.exports = function Route(app, passport) {
                         mem: result[0],
                         disk: result[1],
                         network: result[2],
-                        processes: result[3],
+                        processes: result[3].list.slice(0, 5),
                         user: result[4],
                         notification: result[5]
                     })
