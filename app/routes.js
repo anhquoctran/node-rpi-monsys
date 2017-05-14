5776d6380d85177c1bf203ee7c1943ba78aa8e86
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2017-05-14 21:07:51 +0700

    sdfsdfdf

--- app/routes.js
+++ app/routes.js
@@ -93,6 +93,7 @@ module.exports = function Route(app, passport) {
                             migrator.getOneUser(req.session.user.username), migrator.getNotification(req.session.user.username), migrator.getFirstActivities(req.session.user.username)
                         ])
                         .then(data => {
+                            console.log(data[0])
                             res.render("layouts/profile/profile", {
                                 title: "Profile - RPiMonSys",
                                 user: data[0],
