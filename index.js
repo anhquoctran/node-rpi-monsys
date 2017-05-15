b8b3dbbb5875e65e851ae39ffda38d1f3230bef7
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2017-05-16 00:00:41 +0700

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var server = require('http').Server(app)
 var port = process.env.PORT || 3927
-server.listen(3000, function() {
+server.listen(port, function() {
     console.log("Application is running at " + port)
 })
 
