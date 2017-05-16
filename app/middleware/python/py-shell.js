9069b7ca831980b25cee92bd7eb774d6b0f49cee
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2017-05-16 09:28:34 +0700

    sdfdf

--- app/middleware/python/py-shell.js
+++ app/middleware/python/py-shell.js
@@ -4,7 +4,7 @@ function PythonExecution() {
 
     this.getCpuUsage = function() {
         return new Promise((resolve, reject) => {
-            var pyshell = new PythonShell("/" + __dirname + "/cpu_usage.py")
+            var pyshell = new PythonShell.run("cpu_usage.py", { scriptPath: __dirname })
             pyshell.on('message', function(message) {
                 resolve(message)
             })
