61b89b79f26ca26169a442627625ae52cd631a20
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2017-05-17 11:26:24 +0700

    removed static html

--- app/middleware/python/cpu_usage.py
+++ app/middleware/python/cpu_usage.py
@@ -1,12 +1,14 @@
 import psutil
 import sys
+import threading
+import time
 
 def get_cpu_usage():
     while True:
         result = psutil.cpu_percent(interval=1, percpu=False)
         print(result)
         sys.stdout.flush()
+        time.sleep(1)
 
 if __name__ == "__main__":
     get_cpu_usage()
-    
