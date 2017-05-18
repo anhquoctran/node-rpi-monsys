6279e0c255cb510a5e928a9da5c17ed32a092e05
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2017-05-18 08:29:29 +0700

    fix socket

--- public/js/socketProcessor.js
+++ public/js/socketProcessor.js
@@ -42,66 +42,68 @@ $(document).ready(function() {
         }
     }
 
-    Highcharts.chart("cpu-usage-chart", {
-        chart: {
-            zoomType: 'x'
-        },
-        title: {
-            text: 'CPU Percentage (%)'
-        },
-        subtitle: {
-            text: document.ontouchstart === undefined ?
-                'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-        },
-        xAxis: {
-            type: 'datetime'
-        },
-        yAxis: {
-            title: {
-                text: 'Percentage (%)'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            area: {
-                fillColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
-                    },
-                    stops: [
-                        [0, Highcharts.getOptions().colors[0]],
-                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                    ]
-                },
-                marker: {
-                    radius: 2
-                },
-                lineWidth: 1,
-                states: {
-                    hover: {
-                        lineWidth: 1
+    socket.on("cpu", function(cpu) {
+        var data = []
+        data.push(cpu)
+        Highcharts.chart("cpu-usage-chart", {
+            chart: {
+                zoomType: 'x',
+                events: {
+                    load: function() {
+                        chart = this
                     }
-                },
-                threshold: null
-            }
-        },
-        series: [{
-            type: 'area',
-            name: 'Percent',
-            data: (function() {
-                socket.on("cpu", function(cpu) {
-                    console.log("CPU: " + cpu)
-                    data = []
-                    data.push(cpu)
-
-                })
-            })
-        }]
+                }
+            },
+            title: {
+                text: 'CPU Percentage (%)'
+            },
+            subtitle: {
+                text: document.ontouchstart === undefined ?
+                    'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Percentage (%)'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+            series: [{
+                type: 'area',
+                name: 'Percent',
+                data: data
+            }]
+        })
     })
 
     socket.on('logical', function(logical) {
